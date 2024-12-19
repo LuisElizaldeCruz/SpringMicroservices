33fb33fa81899464f38ad5b0bfe1b7eb414f1a34
LuisElizaldeCruz <luis.elizalde.dev@gmail.com>
2024-12-18 21:37:56 -0600

    se configuro el api gateway

--- microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
+++ microservice-course/src/main/java/com/microservice/course/client/StudentClient.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name = "msvc-student", url = "localhost:8090/api/student")//cual es el microservicio al que vamos a consultar
+@FeignClient(name = "msvc-student", url = "localhost:8080/api/student")//cual es el microservicio al que vamos a consultar
 public interface StudentClient {
 
     @GetMapping("/search-my-course/{idCourse}")//para acceder al endpoint de la peticion en donde se comunica con el curso
